0c324fbc296dad6345473ebda6cbb4d2c3cb3c64
Danylo Velizhanskyi <velizhanskiy1994@gmail.com>
2020-07-11 18:17:00 +0200

    create app github-users

---
+++ src/components/Search.js
@@ -0,0 +1,34 @@
+import React, {useContext, useState} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
+
+export const Search = () => {
+    const [value, setValue] = useState('')
+    const {show} = useContext(AlertContext)
+
+    const onSubmit = event => {
+        if (event.key !== 'Enter') {
+            return
+        }
+
+        if (value.trim()) {
+            console.log('Make request with: ', value)
+        } else {
+          show('Please enter username!')  
+        }
+
+    }
+
+
+    return (
+        <div className="form-group">
+           <input 
+                type="text"
+                className="form-control"
+                placeholder="Please enter username..."
+                value={value}
+                onChange={event => setValue(event.target.value)}
+                onKeyPress={onSubmit}
+           />         
+        </div>
+    )
+}
